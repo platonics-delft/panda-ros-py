f02bc152e94a3ec8c1e11fb49fa32f05bca83d45
giovannifranzese94 <giovanni.franzese94@gmail.com>
2024-01-26 15:02:33 +0100

    removed ad old print

--- panda_ros/panda.py
+++ panda_ros/panda.py
@@ -148,7 +148,6 @@ class Panda():
             q_start.w=-q_start.w
         inner_prod=q_start.x*q_goal.x+q_start.y*q_goal.y+q_start.z*q_goal.z+q_start.w*q_goal.w
         theta= np.arccos(np.abs(inner_prod))
-        print(theta)
         
         step_num_polar = math.floor(theta / interp_dist_polar)
         
